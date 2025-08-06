3d445614bf20511368c199e28014038d5b78e776
Sherif <sherifnabil441@gmail.com>
2025-08-06 19:55:02 +0300

    adding data transfer object

---
+++ app/Http/DTOs/InventoryItemSearchDTO.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\DTOs;
+
+readonly class InventoryItemSearchDTO implements DTOContract
+{
+    public function __construct(
+        public ?string $name,
+        public ?float $min_price,
+        public ?float $max_price,
+    ) {}
+}
